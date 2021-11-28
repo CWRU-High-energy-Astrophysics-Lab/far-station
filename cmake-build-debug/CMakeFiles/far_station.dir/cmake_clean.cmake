849b317e50b42f91d8fadd78a88303cc5a7be8a7
Robin James <70821234+Rjames13@users.noreply.github.com>
2021-11-28 13:46:15 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/far_station.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/far_station.dir/main.cpp.o"
+  "far_station"
+  "far_station.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/far_station.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
