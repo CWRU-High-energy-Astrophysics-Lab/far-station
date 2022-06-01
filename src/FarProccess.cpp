86dcb969dfc4e95a43160dab36833316c9c1a239
robin <rnj13@gmail.com>
2022-06-01 11:50:22 -0400

    cdas

--- src/FarProccess.cpp
+++ src/FarProccess.cpp
@@ -341,22 +341,26 @@ Generalmsg decrypt(const std::string& input) {
     std::cout<< input <<std::endl;
     std::string type = input.substr(0, 4);
     unsigned long headerend = input.find(':');
-    std::string payload = input.substr(headerend);
-    if (type == "T3LI") {
-        msg = T3msg(payload);
-    } else if (type.substr(0, 2) == "CMD") {
-        msg = Cmdmsg(payload, type.at(3));
-    } else if (type == "HIST") {
-        msg = MsgHistory(payload);
-    } else if (type == "T2LI") {
-        msg = T2msg(payload);
-    } else if (type == "LOGA") {
-        msg = Logmsg(payload);
-    } else {
-        msg = Generalmsg(type, "REV0", payload, 12);
-        //add a report to log
+    try {
+        std::string payload = input.substr(headerend + 1);
+        if (type == "T3LI") {
+            msg = T3msg(payload);
+        } else if (type.substr(0, 2) == "CMD") {
+            msg = Cmdmsg(payload, type.at(3));
+        } else if (type == "HIST") {
+            msg = MsgHistory(payload);
+        } else if (type == "T2LI") {
+            msg = T2msg(payload);
+        } else if (type == "LOGA") {
+            msg = Logmsg(payload);
+        } else {
+            msg = Generalmsg(type, "REV0", payload, 12);
+
+
+        }
     }
-
-
+    catch (const std::out_of_range) {
+        msg = Generalmsg("ERRO", "REV0", input, 12);
+    }
     return msg;
 }
