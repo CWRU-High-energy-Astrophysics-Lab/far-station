de15d5502a6ab0edeca6417bbfd22dbeaaef65d3
robin <rnj13@gmail.com>
2022-06-14 12:10:24 -0400

    cdas

--- launch.sh
+++ launch.sh
@@ -3,4 +3,4 @@
 cd ~/far-station || cd ~/CLionProjects/far-station || exit;#
 cmake CMakeLists.txt;#
 make;#
-./~/far-station/far_station || ./~/CLionProjects/far-station/far_station || exit;#
+./far_station || exit;#
